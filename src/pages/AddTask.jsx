c5dd80796983c0366069afa8fd0efd05b7eece5a
nahid-dev <nahidv232@gmail.com>
2024-02-08 22:15:41 +0600

    tailwind and all page setup done

---
+++ src/pages/AddTask.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddTask = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">Add A Task</h1>
+    </div>
+  );
+};
+
+export default AddTask;
---
+++ src/pages/AllTask.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AllTask = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">All Task List Here!!!</h1>
+    </div>
+  );
+};
+
+export default AllTask;
